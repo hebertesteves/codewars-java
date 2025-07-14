7c79cc3abd88b2cbc225b81a72b39cef2a3e2525
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-07-14 17:16:56 -0300

    feat: adiciona solução para DoubleInteger

---
+++ src/kyu8/DoubleInteger.java
@@ -0,0 +1,11 @@
+package kyu8;
+
+public class DoubleInteger {
+    public static int doubleInteger(int i) {
+        return i * 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(doubleInteger(2));
+    }
+}
