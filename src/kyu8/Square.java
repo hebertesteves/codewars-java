f22ed2b53ee41893e0097f028cc35f059bdc85ac
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-07-19 17:32:24 -0300

    feat: adiciona solução para Square

---
+++ src/kyu8/Square.java
@@ -0,0 +1,13 @@
+package kyu8;
+
+public class Square {
+    public static int square(int n){
+        return n * n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(square(2));
+        System.out.println(square(50));
+        System.out.println(square(1));
+    }
+}
