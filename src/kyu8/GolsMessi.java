fa4764ead1a1ec4759e472911f02f358e9f88711
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-07-23 23:18:42 -0300

    feat: adiciona solução para GolsMessi

---
+++ src/kyu8/GolsMessi.java
@@ -0,0 +1,8 @@
+package kyu8;
+
+public class GolsMessi {
+    public static int laLigaGoals = 43;
+    public static int championsLeagueGoals = 10;
+    public static int copaDelReyGoals = 5;
+    public static int totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+}
