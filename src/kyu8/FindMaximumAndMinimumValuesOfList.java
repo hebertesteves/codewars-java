31d24f6adc3c558387f65284d6df66a077b624d3
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-09-01 16:40:38 -0300

    feat: adiciona solução para FindMaximumAndMinimumValuesOfList

---
+++ src/kyu8/FindMaximumAndMinimumValuesOfList.java
@@ -0,0 +1,13 @@
+package kyu8;
+
+import java.util.Arrays;
+
+public class FindMaximumAndMinimumValuesOfList {
+    public int min(int[] list) {
+        return Arrays.stream(list).min().getAsInt();
+    }
+
+    public int max(int[] list) {
+        return Arrays.stream(list).max().getAsInt();
+    }
+}
