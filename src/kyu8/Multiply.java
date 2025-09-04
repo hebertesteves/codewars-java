79bf359411de9c6a972290690a710ff8e9b99d82
Hebert Esteves <108402431+hebertesteves@users.noreply.github.com>
2025-09-04 19:10:45 -0300

    feat: adiciona solução para Multiply

---
+++ src/kyu8/Multiply.java
@@ -0,0 +1,12 @@
+package kyu8;
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiply(1.0, 1.0));
+        System.out.println(multiply(2.0, 1.0));
+    }
+}
