6d3d4d5e44d6d0cfa37cb90c85da596a5832d9f9
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-09-29 19:28:24 -0300

    feat: adiciona solução para MakeUpperCase

---
+++ src/kyu8/MakeUpperCase.java
@@ -0,0 +1,11 @@
+package kyu8;
+
+public class MakeUpperCase {
+    public static String makeUpperCase(String str) {
+        return str.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(makeUpperCase("hello"));
+    }
+}
