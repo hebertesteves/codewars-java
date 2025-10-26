28bc8e00926e05dafe183063138cf1009c40ead0
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-10-26 19:10:45 -0300

    feat: adiciona solução para DebugCelsiusConverter

---
+++ src/kyu8/DebugCelsiusConverter.java
@@ -0,0 +1,17 @@
+package kyu8;
+
+public class DebugCelsiusConverter {
+    public static String weatherInfo(int temp) {
+        double c = convertToCelsius(temp);
+        if (c < 0) return c + " is freezing temperature";
+        else return c + " is above freezing temperature";
+    }
+
+    public static double convertToCelsius(int temperature) {
+        return (temperature - 32) * (5/9.0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(weatherInfo(50));
+    }
+}
