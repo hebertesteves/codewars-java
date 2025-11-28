7b3961dd38394c24a9122a5e0de872fe29f0e723
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-11-28 20:00:00 -0300

    feat: adiciona solução para FindMissingNumber

--- src/kyu7/FindMissingNumber.java
+++ src/kyu7/FindMissingNumber.java
@@ -1,4 +1,16 @@
 package kyu7;
 
 public class FindMissingNumber {
+    public static int missingNo(int[] nums) {
+        int N = 100;
+
+        int somaEsperada = N * (N + 1) / 2;
+
+        int somaReal = 0;
+        for (int num : nums) {
+            somaReal += num;
+        }
+
+        return somaEsperada - somaReal;
+    }
 }
