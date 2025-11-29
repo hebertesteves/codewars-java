5291fe04ea6fbae6958cfb121f7f12671a15f700
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-11-29 13:00:00 -0300

    feat: adiciona solução para LeapYears

--- src/kyu7/LeapYears.java
+++ src/kyu7/LeapYears.java
@@ -1,4 +1,12 @@
 package kyu7;
 
 public class LeapYears {
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) return year % 400 == 0;
+            else return true;
+        } else {
+            return false;
+        }
+    }
 }
