156687af04f7194001297063e510d78a6ea96603
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-12-18 22:50:11 -0300

    feat: adiciona solução para TwoOldestAges

--- src/kyu7/TwoOldestAges.java
+++ src/kyu7/TwoOldestAges.java
@@ -1,4 +1,11 @@
 package kyu7;
 
+import java.util.*;
+
 public class TwoOldestAges {
+    public static int[] twoOldestAges(int[] ages) {
+        List<Integer> list = new ArrayList<>(Arrays.stream(ages).boxed().toList());
+        Collections.sort(list);
+        return new int[]{list.get(list.size() - 2), list.get(list.size() - 1)};
+    }
 }
