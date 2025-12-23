633027d2e8ae493d3f25364ff5b949a46308c8b0
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-12-22 23:23:06 -0300

    feat: adiciona solução para DontGiveMeFive

---
+++ src/kyu7/DontGiveMeFive.java
@@ -0,0 +1,15 @@
+package kyu7;
+
+public class DontGiveMeFive {
+    public static int dontGiveMeFive(int start, int end) {
+        int count = 0;
+
+        for (int i = start; i <= end; i++) {
+            if (!String.valueOf(i).contains("5")) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
