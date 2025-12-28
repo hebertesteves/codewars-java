6c4c25c110639fbbbc906e18673f33f807577e63
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-12-27 23:35:30 -0300

    feat: adiciona solução para TrafficLights

--- src/kyu8/TrafficLights.java
+++ src/kyu8/TrafficLights.java
@@ -1,4 +1,7 @@
 package kyu8;
 
 public class TrafficLights {
+    public static String updateLight(String current) {
+        return current.equalsIgnoreCase("red") ? "green" : current.equalsIgnoreCase("green") ? "yellow" : "red";
+    }
 }
