6284a06a23d687992fe27f2c756ebe9a374011b2
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-12-30 23:09:08 -0300

    feat: adiciona solução para MessiGoalsFunction

---
+++ src/kyu8/MessiGoalsFunction.java
@@ -0,0 +1,7 @@
+package kyu8;
+
+public class MessiGoalsFunction {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+}
