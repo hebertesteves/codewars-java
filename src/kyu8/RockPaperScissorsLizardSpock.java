fc03304815e993e7f28132416bbb1ffc4918d1a3
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-01-18 19:09:00 -0300

    feat: adiciona solução para RockPaperScissorsLizardSpock

--- src/kyu8/RockPaperScissorsLizardSpock.java
+++ src/kyu8/RockPaperScissorsLizardSpock.java
@@ -1,4 +1,26 @@
 package kyu8;
 
+import java.util.Map;
+import java.util.Set;
+
 public class RockPaperScissorsLizardSpock {
+    public static String rpsls(String player1, String player2){
+        Map<String, Set<String>> regras = Map.of(
+                "rock", Set.of("lizard", "scissors"),
+                "paper", Set.of("rock", "spock"),
+                "scissors", Set.of("lizard", "paper"),
+                "lizard", Set.of("paper", "spock"),
+                "spock", Set.of("scissors", "rock")
+        );
+
+        if (regras.get(player1).contains(player2)) {
+            return "Player 1 Won!";
+        }
+
+        if (regras.get(player2).contains(player1)) {
+            return "Player 2 Won!";
+        }
+
+        return "Draw!";
+    }
 }
