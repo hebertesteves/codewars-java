bc7949c7c15a0c490268f8e042c7a708ba434e39
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-01-31 09:03:00 -0300

    feat: adiciona solução para RockPaperScissorsLizardSpock

---
+++ src/kyu7/FunctionalAddition.java
@@ -0,0 +1,9 @@
+package kyu7;
+
+import java.util.function.IntUnaryOperator;
+
+public class FunctionalAddition {
+    public static IntUnaryOperator add(int n) {
+        return x -> x + n;
+    }
+}
