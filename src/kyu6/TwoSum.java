7fa961e4eba8fee6835e1d5bb7a73fbb43592efc
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-04-23 21:37:55 -0300

    feat: adiciona solução para TwoSum

---
+++ src/kyu6/TwoSum.java
@@ -0,0 +1,12 @@
+package kyu6;
+
+public class TwoSum {
+    public static int[] twoSum(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] == target - numbers[i]) return new int[]{i, j};
+            }
+        }
+        return null;
+    }
+}
