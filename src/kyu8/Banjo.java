67dce5a923f878af99e0bdb25a3844a03891beea
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-05-13 22:46:38 -0300

    feat: adiciona solução para Banjo

---
+++ src/kyu8/Banjo.java
@@ -0,0 +1,7 @@
+package kyu8;
+
+public class Banjo {
+    public static String areYouPlayingBanjo(String name) {
+        return name.toLowerCase().charAt(0) == 'r' ? name + " plays banjo" : name + " does not play banjo";
+    }
+}
